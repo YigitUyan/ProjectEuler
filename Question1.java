65f2fa3bd70c9f0467223b3d7f4c1fa7dc45cb25
Yigit Uyan <git@yigituyan.com>
2020-01-04 00:31:13 -0800

    Question 1 Solution
    - Added solution for question 1.

---
+++ Question1.java
@@ -0,0 +1,21 @@
+
+// Project Euler - Question 1:
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+public class Question1
+{
+    public static void main(String [] _Arguments)
+    {
+        int sum = 0;
+
+        for(int i = 0; i < 1000; i++)
+        {
+            if(i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        System.out.println("Answer: " + sum);
+    }
+}
